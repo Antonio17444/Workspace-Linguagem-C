66e6a1a7da48bf2eba40a42dcd9efd54bedb0ae3
Antonio Henrique <antoniohenrique1305l@gmail.com>
2025-02-18 11:11:52 -0300

    Alteração

--- Laços de repetição/tabuada_de_um_num.c
+++ Laços de repetição/tabuada_de_um_num.c
@@ -7,7 +7,7 @@ int main(){
     printf("Informe um numero: ");
     scanf("%d",&num);
 
-    for (int i = 1; i <= num; i++){
+    for (int i = 1; i <= 10; i++){
         
         printf("%.2d * %d = %.2d\n", num, i, num*i);
 
