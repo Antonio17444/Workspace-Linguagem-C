76a54721bb888c7597066f1481a5c036ffb32027
Antonio Henrique <antoniohenrique1305l@gmail.com>
2025-03-09 17:35:51 -0300

    Teste 1 com do while

---
+++ Laços de repetição/testes_com_dowhile.c
@@ -0,0 +1,22 @@
+/*         
+
+              Soma de numeros com Do While
+
+#include <stdio.h>
+
+int main(){
+    int num, soma = 0;
+
+    do{
+        printf("Informe um numero (ou 0 para encerrar a contagem): \n");
+        scanf("%d", &num);
+        soma += num;
+    } while (num != 0);
+
+    printf("A soma dos numeros e: %d\n", soma);
+    return 0;
+}
+
+
+*/
+
