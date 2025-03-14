92e14f6d23806d635b3aea11bf1744c209a6fb9c
Antonio Henrique <antoniohenrique1305l@gmail.com>
2025-03-14 12:39:33 -0300

    printando vetor inverso

---
+++ Vetores/vetores_inversos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int vetor[10];
+
+    for(int i = 0; i<10; i++){
+        printf("Informe um dado: \n");
+        scanf("%d", &vetor[i]);
+    }
+    printf("Dados inseridos no Vetor:\n");
+    for(int i = 9; i>=0; i--){
+        printf("%d\n", vetor[i]);
+    }
+    printf("Fim...");
+
+    return 0;
+}
